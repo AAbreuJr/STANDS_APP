31bf44fe244630965cf8f5ef26545cbc22d1e8e6
AndersonAbreuJr <68236072+AndersonAbreuJr@users.noreply.github.com>
2020-10-12 15:23:34 -0400

    protect routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
   path('', views.landing, name='landing'),
   path('stands/', views.stands_index, name='index'),
+  path('about/', views.about, name='about'),
   path('stands/create/', views.StandCreate.as_view(), name='stands_create'),
   path('stands/<int:stand_id>/', views.stands_detail, name='detail'),
   path('stands/<int:pk>/update/', views.StandUpdate.as_view(), name='stands_update'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,30 +1,54 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Stand
 
 # Create your views here.
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid credentials - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
 def landing(request):
     return render(request, 'landing.html')
 
+@login_required
 def stands_index(request):
     stands = Stand.objects.filter()
     return render(request, 'stands/index.html', {'stands': stands})
 
-class StandCreate(CreateView):
+
+class StandCreate(LoginRequiredMixin, CreateView):
     model = Stand
     fields = '__all__'
 
-
+@login_required
 def stands_detail(request, stand_id):
     stand = Stand.objects.get(id=stand_id)
     return render(request, 'stands/detail.html', {
         'stand': stand
     })
 
-class StandUpdate(UpdateView):
+class StandUpdate(LoginRequiredMixin, UpdateView):
     model = Stand
     fields = '__all__'
 
-class StandDelete(DeleteView):
+class StandDelete(LoginRequiredMixin, DeleteView):
     model = Stand
     success_url = '/stands/'
+
+def about(request):
+    return render(request, 'about.html')
