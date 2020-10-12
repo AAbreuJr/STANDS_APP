66df8b2cdd3d3e60fd5561c07b7d9b4cc5d69559
AndersonAbreuJr <68236072+AndersonAbreuJr@users.noreply.github.com>
2020-10-12 13:59:12 -0400

    add cancel edit and delete features

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
   path('', views.landing, name='landing'),
   path('stands/', views.stands_index, name='index'),
+  path('stands/create/', views.StandCreate.as_view(), name='stands_create'),
+  path('stands/<int:stand_id>/', views.stands_detail, name='detail'),
+  path('stands/<int:pk>/update/', views.StandUpdate.as_view(), name='stands_update'),
+  path('stands/<int:pk>/delete/', views.StandDelete.as_view(), name='stands_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Stand
 
 # Create your views here.
 def landing(request):
     return render(request, 'landing.html')
 
 def stands_index(request):
-    return render(request, 'stands/index.html')
+    stands = Stand.objects.filter()
+    return render(request, 'stands/index.html', {'stands': stands})
 
+class StandCreate(CreateView):
+    model = Stand
+    fields = '__all__'
+
+
+def stands_detail(request, stand_id):
+    stand = Stand.objects.get(id=stand_id)
+    return render(request, 'stands/detail.html', {
+        'stand': stand
+    })
+
+class StandUpdate(UpdateView):
+    model = Stand
+    fields = '__all__'
+
+class StandDelete(DeleteView):
+    model = Stand
+    success_url = '/stands/'
